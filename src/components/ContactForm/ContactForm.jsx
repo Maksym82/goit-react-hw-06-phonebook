c3add1b469073f3d47d2536ac6c33ffcc90811e5
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-11 21:38:55 +0300

    update ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,74 +1,62 @@
 import { Form, FormLabel, FormInput, FormButton } from './ContactForm.styled';
 import { useDispatch, useSelector } from 'react-redux';
+import { getContacts } from 'redux/selectors';
+import Notiflix from 'notiflix';
+import { addContact } from 'redux/contactsSlice';
 
 
 
-const ContactForm = ({ onSubmit }) => {
-    const [name, setName] = useState('');
-    const [number, setNumber] = useState('');
+export function ContactForm() {
+    const contacts = useSelector(getContacts);
+    const dispatch = useDispatch();
 
-
-    const nameInputId = nanoid();
-    const telInputId = nanoid();
-
-
-    const handleChange = event => {
-        const { name, value } = event.target;
-        switch (name) {
-          case 'name':
-            setName(value);
-            break;
-          case 'number':
-            setNumber(value);
-            break;
-          default:
-            return;
+    const handleSubmit = evt => {
+        evt.preventDefault();
+        const form = evt.target;
+        const { name, number } = form.elements;
+    
+        if (contacts.some(contact => contact.name === name.value)) {
+          Notiflix.Report.warning(
+            'Warning',
+            `${name.value} is already in contacts.`
+          );
+        } else {
+          dispatch(addContact(name.value, number.value));
         }
+        form.reset();
     };
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        onSubmit ({ name, number });
-            setName('');
-            setNumber('');
-    }
-        
-    
-   
     return (
-        <Form onSubmit = {handleSubmit}>
-            <Label htmlFor = {nameInputId}>
-                 Name
-                <Input 
-                    type = 'text'
-                    name = 'name'
-                    value = {name}
-                    onChange = {handleChange}
-                    pattern= "^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        <Form onSubmit={handleSubmit}>
+            <FormLabel>
+                Name
+                <FormInput
+                    type='text'
+                    name='name'
+                    pattern="^[a-zA-Zа-яА-Я\s]+$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                     required
-                />    
-            </Label>
-            <Label htmlFor = {telInputId}>
-                Phone number
-                <Input 
-                    type = 'tell'
-                    name = 'number'
-                    value = {number}
-                    onChange = {handleChange}
-                    pattern= "\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+                />
+            </FormLabel>
+            <FormLabel>
+                Number
+                <FormInput
+                    type="tel"
+                    name="number"
+                    pattern="\+?[0-9\s\-\(\)]+"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                     required
-                />    
-            </Label>
-            <Button type ='submit'>Add contact</Button>
+                />
+            </FormLabel>
+            <FormButton type='submit'>Add Contact</FormButton>              
         </Form>
-    )
-    
-} 
-
-Form.propTypes = {
-    onSubmit: PropTypes.func.isRequired,
+    );
 }
 
-export default ContactForm;
+    
+
+    
+    
+   
+  
+
