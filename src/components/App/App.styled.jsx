4f6c1b4c5aeccaa077c78bc922de7ce16e331c4d
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-11 21:15:07 +0300

    update style

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -4,19 +4,8 @@ export const Container = styled.div`
     width: 500px;
     margin: 0 auto;
     padding: 20px;
+    background-color: blue;
+    border-radius: 6px;
+    
 `;
 
-export const Title = styled.h1`
-    text-align: center;
-    color: blue;
-`;
-
-export const SubTitle = styled.h2`
-    text-align: center;
-    color: blue;
-`;
-
-export const Wrapper = styled.div`
-    text-align: center;
-    font-size: 10px;
-`;
